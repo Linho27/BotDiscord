972d1183b1f6ec9493cf6c7d4f92e3248c8d9897
Linho <69125386+Linho27@users.noreply.github.com>
2024-12-12 15:48:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,26 +34,7 @@ async def hello(ctx):
 
 @bot.event
 async def on_member_update(before: discord.Member, after: discord.Member):
-    if before.roles != after.roles:
-        log_channel = bot.get_channel(LOG_CHANNEL)
-        if log_channel:
-            # Obtendo os cargos adicionados e removidos
-            before_roles = set(before.roles)
-            after_roles = set(after.roles)
-
-            added_roles = after_roles - before_roles
-            removed_roles = before_roles - after_roles
-
-            changes = []
-            if added_roles:
-                changes.append(f'**Adicionados:** {", ".join(role.name for role in added_roles)}')
-            if removed_roles:
-                changes.append(f'**Removidos:** {", ".join(role.name for role in removed_roles)}')
-
-            if changes:
-                await log_channel.send(
-                    f"Alteração de cargos para {after.mention}:\n" + "\n".join(changes)
-                )
+    await log_channel.send('Cargos Alterados')
 
 
 
